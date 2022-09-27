ba2c3bb238b032c78efa6f8fcae8a1fb526c1df4
Chariton Kypraios <haritos19@gmail.com>
2022-09-27 19:40:44 +0300

    Allages register

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -39,78 +39,84 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
-            this.label3.Location = new System.Drawing.Point(327, 108);
+            this.label3.Font = new System.Drawing.Font("Calibri", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label3.Location = new System.Drawing.Point(418, 92);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(110, 20);
+            this.label3.Size = new System.Drawing.Size(113, 27);
             this.label3.TabIndex = 11;
-            this.label3.Text = "LOGIN FORM";
+            this.label3.Text = "Login Form";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(331, 180);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.textBox2.Location = new System.Drawing.Point(344, 260);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
             this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(110, 20);
+            this.textBox2.Size = new System.Drawing.Size(262, 27);
             this.textBox2.TabIndex = 10;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(266, 180);
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label2.Location = new System.Drawing.Point(340, 239);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.Size = new System.Drawing.Size(71, 19);
             this.label2.TabIndex = 9;
             this.label2.Text = "password";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(277, 150);
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label1.Location = new System.Drawing.Point(340, 174);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.Size = new System.Drawing.Size(123, 19);
             this.label1.TabIndex = 8;
-            this.label1.Text = "user";
+            this.label1.Text = "username / email";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(331, 150);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.textBox1.Location = new System.Drawing.Point(344, 195);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(110, 20);
+            this.textBox1.Size = new System.Drawing.Size(262, 27);
             this.textBox1.TabIndex = 7;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(340, 222);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(344, 312);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(88, 37);
+            this.button1.Size = new System.Drawing.Size(125, 37);
             this.button1.TabIndex = 6;
             this.button1.Text = "login";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form1
+            // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(888, 482);
+            this.ClientSize = new System.Drawing.Size(909, 556);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.Name = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Login";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
--- Register.cs
+++ Register.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Data.SQLite;
 using System.Windows.Forms;
 
 namespace TaxeiaAnaptuksi_Gerovasilis_Kypraios
 {
     public partial class Register : Form
     {
+        String connectionString = "Data source =Users.db;Version=3";
+        SQLiteConnection connection;
+
         public Register()
         {
             InitializeComponent();
@@ -12,7 +16,55 @@ namespace TaxeiaAnaptuksi_Gerovasilis_Kypraios
 
         private void Register_Load(object sender, EventArgs e)
         {
+            connection = new SQLiteConnection(connectionString);
+        }
 
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            connection.Open();
+            String email = textBox1.Text;
+            String password = textBox2.Text;
+            String firstName = textBox3.Text;
+            String lastName = textBox4.Text;
+
+            try
+            {
+                String selectSQL = "SELECT * from Users where email='" + email + "'";
+                SQLiteCommand selectCommand = new SQLiteCommand(selectSQL, connection);
+                SQLiteDataReader reader = selectCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var result = reader.GetString(3);
+                    if (result.ToString().Trim() == email)
+                    {
+                        MessageBox.Show("The email " + email + " you used, already exists!");
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurs. Please try again later!");
+            }
+
+            try
+            {
+                String insertSQL = "Insert into Users(email,password,firstName,lastName) " +
+                "values('" + email + "','" + password + "','" + firstName + "','" + lastName + "')";
+                SQLiteCommand cmd = new SQLiteCommand(insertSQL, connection);
+                int count = cmd.ExecuteNonQuery();
+                if (count > 0)
+                {
+                    MessageBox.Show("Successfully registered");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurs. Please try again later!");
+            }
+
+            connection.Close();
         }
     }
 }
