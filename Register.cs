fb70ccecb571507fc0568fbd54ab6b02fd246820
KostasGer <gerobillkostas1998@gmail.com>
2022-10-15 13:24:12 +0300

    changes kostas

--- Register.cs
+++ Register.cs
@@ -66,5 +66,25 @@ namespace TaxeiaAnaptuksi_Gerovasilis_Kypraios
 
             connection.Close();
         }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
