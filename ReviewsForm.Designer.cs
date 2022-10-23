b277ec0f7ec203509e9c115afae77a5867dd25bf
Chariton Kypraios <chkypraios@allweb.gr>
2022-10-23 22:15:42 +0300

    reviews final

--- ReviewsForm.Designer.cs
+++ ReviewsForm.Designer.cs
@@ -32,6 +32,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.title = new System.Windows.Forms.TextBox();
             this.body = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -46,7 +48,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 278);
+            this.button1.Location = new System.Drawing.Point(12, 236);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(126, 35);
             this.button1.TabIndex = 1;
@@ -56,24 +58,46 @@
             // 
             // title
             // 
-            this.title.Location = new System.Drawing.Point(12, 104);
+            this.title.Location = new System.Drawing.Point(12, 107);
             this.title.Name = "title";
             this.title.Size = new System.Drawing.Size(771, 20);
             this.title.TabIndex = 2;
             // 
             // body
             // 
-            this.body.Location = new System.Drawing.Point(12, 130);
+            this.body.Location = new System.Drawing.Point(12, 158);
             this.body.Name = "body";
-            this.body.Size = new System.Drawing.Size(771, 142);
+            this.body.Size = new System.Drawing.Size(771, 72);
             this.body.TabIndex = 3;
             this.body.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label2.Location = new System.Drawing.Point(12, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Title";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label4.Location = new System.Drawing.Point(12, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 19);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Body";
             // 
             // ReviewsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1150, 683);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.body);
             this.Controls.Add(this.title);
             this.Controls.Add(this.button1);
@@ -92,5 +116,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox title;
         private System.Windows.Forms.RichTextBox body;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
     }
 }
--- ReviewsForm.cs
+++ ReviewsForm.cs
@@ -6,11 +6,13 @@ namespace TaxeiaAnaptuksi_Gerovasilis_Kypraios
 {
     public partial class ReviewsForm : Form
     {
-        String connectionString = "Data source =Users.db;Version=3;Pooling=true";
-        String connectionString2 = "Data source =Reviews.db;Version=3;Pooling=true";
+        string connectionString = "Data source =Users.db;Version=3;Pooling=true";
+        string connectionString2 = "Data source =Reviews.db;Version=3;Pooling=true";
         SQLiteConnection connection;
         SQLiteConnection connection2;
-        String user;
+        string user;
+        Boolean isUser = false;
+
         public ReviewsForm(String user)
         {
             InitializeComponent();
@@ -20,13 +22,20 @@ namespace TaxeiaAnaptuksi_Gerovasilis_Kypraios
                 button1.Visible = false;
                 title.Visible = false;
                 body.Visible = false;
+                label2.Visible = false;
+                label4.Visible = false;
             }
             else
             {
                 title.Visible = true;
                 body.Visible = true;
                 button1.Visible = true;
+                label2.Visible = true;
+                label4.Visible = true;
+                isUser = true;
             }
+
+
         }
 
         private void ReviewsForm_Load(object sender, EventArgs e)
@@ -34,18 +43,22 @@ namespace TaxeiaAnaptuksi_Gerovasilis_Kypraios
 
             connection = new SQLiteConnection(connectionString);
             connection2 = new SQLiteConnection(connectionString2);
+
+            ReviewsLoad();
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             connection.Open();
-            String title = this.title.Text;
-            String description = this.body.Text;
+            string title = this.title.Text;
+            string description = this.body.Text;
             int usersId = 0;
+            string usersUsername = "";
 
             try
             {
-                String selectSQL = "SELECT * from Users where email='" + this.user + "'";
+                string selectSQL = "SELECT * from Users where email='" + this.user + "'";
                 SQLiteCommand selectCommand = new SQLiteCommand(selectSQL, connection);
                 SQLiteDataReader reader = selectCommand.ExecuteReader();
 
@@ -55,6 +68,11 @@ namespace TaxeiaAnaptuksi_Gerovasilis_Kypraios
                     if (result.ToString().Trim() == this.user)
                     {
                         usersId = reader.GetInt32(0);
+                        usersUsername = reader.GetString(1);
+                    }
+                    else
+                    {
+                        return;
                     }
                 }
             }
@@ -62,15 +80,12 @@ namespace TaxeiaAnaptuksi_Gerovasilis_Kypraios
             {
                 MessageBox.Show("An error occurs. Please try again later!");
             }
-
             connection.Close();
 
             connection2.Open();
-
             try
             {
-
-                String insertSQL = "Insert into Reviews (description,usersId,title) VALUES ('asdasasdadsfgdsfsdd',16,'23asdasd4saddas')";
+                string insertSQL = "Insert into Reviews (description,usersId,title) VALUES ('" + description + "','" + usersId + "','" + title + "','" + usersUsername + "')";
                 SQLiteCommand cmd = new SQLiteCommand(insertSQL, connection2);
                 int count = cmd.ExecuteNonQuery();
                 if (count > 0)
@@ -82,14 +97,89 @@ namespace TaxeiaAnaptuksi_Gerovasilis_Kypraios
             {
                 MessageBox.Show("An error occurs. Please try again later!");
             }
-
-
-
             connection2.Close();
 
-
+            ReviewsLoad();
         }
 
+        private void ReviewsLoad()
+        {
+            connection2.Open();
 
+            try
+            {
+                string selectSQL = "SELECT * from Reviews";
+                SQLiteCommand selectCommand = new SQLiteCommand(selectSQL, connection2);
+                SQLiteDataReader reader = selectCommand.ExecuteReader();
+
+                int counter = 0;
+                if (isUser)
+                {
+                    counter = 1;
+                }
+
+                while (reader.Read())
+                {
+                    var titleToShow = new TextBox();
+                    titleToShow.Location = new System.Drawing.Point(12, 147 + counter * 170);
+                    titleToShow.Name = "title" + counter;
+                    titleToShow.Size = new System.Drawing.Size(771, 20);
+                    titleToShow.TabIndex = 2 + counter;
+                    titleToShow.Text = reader.GetString(1);
+                    this.Controls.Add(titleToShow);
+                    titleToShow.ReadOnly = true;
+
+
+                    var bodyToShow = new RichTextBox();
+                    bodyToShow.Location = new System.Drawing.Point(12, 198 + counter * 170);
+                    bodyToShow.Name = "title" + counter;
+                    bodyToShow.Size = new System.Drawing.Size(771, 72);
+                    bodyToShow.TabIndex = 6 + counter;
+                    bodyToShow.Text = reader.GetString(3);
+                    this.Controls.Add(bodyToShow);
+                    bodyToShow.ReadOnly = true;
+
+                    var userName = new Label();
+                    userName.AutoSize = true;
+                    userName.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+                    userName.Location = new System.Drawing.Point(50, 125 + counter * 170);
+                    userName.Name = "label00" + counter;
+                    userName.Size = new System.Drawing.Size(38, 19);
+                    userName.TabIndex = 7 + counter;
+                    userName.Text = "user " + reader.GetString(4) + " wrote:";
+                    this.Controls.Add(userName);
+
+
+
+                    var titleShowing = new Label();
+                    titleShowing.AutoSize = true;
+                    titleShowing.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+                    titleShowing.Location = new System.Drawing.Point(12, 125 + counter * 170);
+                    titleShowing.Name = "label0" + counter;
+                    titleShowing.Size = new System.Drawing.Size(38, 19);
+                    titleShowing.TabIndex = 4 + counter;
+                    titleShowing.Text = "Title";
+                    this.Controls.Add(titleShowing);
+
+                    var bodyShowing = new Label();
+                    bodyShowing.AutoSize = true;
+                    bodyShowing.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+                    bodyShowing.Location = new System.Drawing.Point(12, 176 + counter * 170);
+                    bodyShowing.Name = "label0" + counter;
+                    bodyShowing.Size = new System.Drawing.Size(41, 19);
+                    bodyShowing.TabIndex = 7 + counter;
+                    bodyShowing.Text = "Body";
+                    this.Controls.Add(bodyShowing);
+
+                    counter += 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurs. Please try again later!");
+            }
+
+            connection2.Close();
+        }
     }
 }
