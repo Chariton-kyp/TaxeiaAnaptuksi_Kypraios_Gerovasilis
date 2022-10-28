60da31597780119844ef703a11d38444fd767939
KostasGer <gerobillkostas1998@gmail.com>
2022-10-28 22:38:37 +0300

    anenewsi

---
+++ MenuSitisis.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace TaxeiaAnaptuksi_Gerovasilis_Kypraios
+{
+    partial class MenuSitisis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label2.Location = new System.Drawing.Point(440, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(258, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Εβδομαδιαίο Μενού Σίτισης";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(178, 142);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(164, 78);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(389, 142);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(164, 78);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(792, 142);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(164, 78);
+            this.panel3.TabIndex = 5;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(590, 142);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(164, 78);
+            this.panel4.TabIndex = 4;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(475, 314);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(164, 78);
+            this.panel5.TabIndex = 7;
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(696, 314);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(164, 78);
+            this.panel6.TabIndex = 6;
+            // 
+            // panel7
+            // 
+            this.panel7.Location = new System.Drawing.Point(254, 314);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(164, 78);
+            this.panel7.TabIndex = 8;
+            // 
+            // MenuSitisis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 541);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Name = "MenuSitisis";
+            this.Text = "MenuSitisis";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+    }
+}
